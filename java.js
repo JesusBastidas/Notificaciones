a2eef96721dc3c8f9a8a8862a84ef7ca54013c97
JesusBastidas <davidbastidascarvajal@gmail.com>
2021-09-22 18:16:54 -0500

    correccion de ventana

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 document.getElementById("abrir_boton").addEventListener(
     "click", function(){
-        document.getElementsByClassName("fondo_invisible")[0].style.display="block"
+        document.getElementsByClassName("fondo_invisible")[0].style.display="block";
         return false
 })
 document.getElementsByClassName("v_modal_close")[0].addEventListener("click",function(){
-    document.getElementsByClassName("fondo_invisible")[0].style.display="none"
+    document.getElementsByClassName("fondo_invisible")[0].style.display="none";
 })
